32b5f0e9c465057f16f4972a338ab5a579a21c30
abogovski <abogovski@gmail.com>
2017-08-23 23:16:42 +0300

    Move example of Fetcherusage to fetcher_test.go

--- main.go
+++ main.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"go-meme-recognizer/fetcher"
-)
-
 func main() {
-	f := fetcher.NewFetcher(10, 10)
-	urlsList := [][]string{[]string{"https://pp.userapi.com/c841030/v841030005/1826e/Bunv2Om-uv4.jpg"},
-		[]string{"https://pp.userapi.com/c638221/v638221662/81171/lLsKjoP3s_E.jpg",
-			"https://pp.userapi.com/c638221/v638221602/55c39/TQnoSaS1eVI.jpg",
-			"https://pp.userapi.com/c638221/v638221602/55c41/nOmwuC6O2mA.jpg",
-			"https://pp.userapi.com/c638221/v638221602/55c48/g0J54ofxdyY.jpg",
-			"https://pp.userapi.com/c638221/v638221602/55c4f/HAipw-io3uY.jpg"},
-		[]string{"https://pp.userapi.com/c638221/v638221007/58c3c/9A0Tz4d06bc.jpg"},
-		[]string{"https://pp.userapi.com/c638221/v638221007/58c32/nuAr6pMJGhs.jpg"},
-		[]string{"https://pp.userapi.com/c638221/v638221007/58c04/KMyDz0wwDIc.jpg"},
-		[]string{"https://pp.userapi.com/c638221/v638221388/5f8a1/_y7dUsi15b8.jpg"},
-		[]string{"https://pp.userapi.com/c837731/v837731337/55cb3/yAsTav_Ap8A.jpg"},
-		[]string{"https://pp.userapi.com/c837731/v837731869/5a3ce/0C9xZypRHRo.jpg"}}
-	queries := fetcher.MakeQueryFromUrlsList("result", urlsList)
-	f.Download(queries, 1)
+
 }
